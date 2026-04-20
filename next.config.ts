0bee687356dca81ac7d272507370806f53ac15f1
Dardan Kabashi <e12510771@student.tuwien.ac.at>
2026-04-20 17:46:04 +0200

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,27 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   eslint: {
-      ignoreDuringBuilds: true,
-  }, typescript: {
-      ignoreBuildErrors: true
-    }
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
